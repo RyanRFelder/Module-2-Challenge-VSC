a39a7f8f8470766426467b8d33fc39332812c9cf
RyanRFelder <ryanrfelder@gmail.com>
2021-01-17 18:15:57 -0800

    Many changes

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ This is a command line application to match applicants with qualifying loans.
 Example:
     $ python app.py
 """
+
+from pathlib import Path
 import sys
 import fire
 import questionary
-from pathlib import Path
 
 from qualifier.utils.fileio import load_csv
 
@@ -19,7 +20,7 @@ from qualifier.utils.calculators import (
 )
 
 from qualifier.filters.max_loan_size import filter_max_loan_size
-from qualifier.filters.credit_score import filter_credit_score
+from qualifier.filters.credit_score import filter_credit_scorep
 from qualifier.filters.debt_to_income import filter_debt_to_income
 from qualifier.filters.loan_to_value import filter_loan_to_value
 
@@ -110,6 +111,7 @@ def save_qualifying_loans(qualifying_loans):
     """
     # @TODO: Complete the usability dialog for savings the CSV Files.
     # YOUR CODE HERE!
+    
 
 
 def run():
